bdafdab126327e72e9128c63aef99281028f3bbe
duttakaushikkd <99733850+duttakaushikkd@users.noreply.github.com>
2026-02-04 00:15:03 +0530

    Initial commit

---
+++ CustomAnswerAgent/src/main/java/com/example/CustomAnswerAgent/CustomAnswerAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.CustomAnswerAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnswerAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnswerAgentApplication.class, args);
+	}
+
+}
---
+++ CustomAnswerAgent/src/test/java/com/example/CustomAnswerAgent/CustomAnswerAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomAnswerAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnswerAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
