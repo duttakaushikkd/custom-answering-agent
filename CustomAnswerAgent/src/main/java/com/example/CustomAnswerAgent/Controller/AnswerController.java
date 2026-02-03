1faa8e911d1399019f8c5347e231a95994aab369
duttakaushikkd <99733850+duttakaushikkd@users.noreply.github.com>
2026-02-04 01:20:18 +0530

    Added code to call openai

--- CustomAnswerAgent/src/main/java/com/example/CustomAnswerAgent/Controller/AnswerController.java
+++ CustomAnswerAgent/src/main/java/com/example/CustomAnswerAgent/Controller/AnswerController.java
@@ -4,6 +4,7 @@ import com.example.CustomAnswerAgent.Service.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,7 @@ public class AnswerController {
     AnswerService answerService;
 
     @GetMapping("/answer")
-    public String getAnswer(String question){
+    public String getAnswer(@RequestParam  String question){
         return answerService.openAiService(question);
     }
 
