41b40161a5d133db04d2b69712c04d084687f51c
duttakaushikkd <99733850+duttakaushikkd@users.noreply.github.com>
2026-02-04 01:57:29 +0530

    Added code to call openai

--- CustomAnswerAgent/src/main/java/com/example/CustomAnswerAgent/OpenaiConfig/OpenAiConfig.java
+++ CustomAnswerAgent/src/main/java/com/example/CustomAnswerAgent/OpenaiConfig/OpenAiConfig.java
@@ -2,8 +2,6 @@ package com.example.CustomAnswerAgent.OpenaiConfig;
 
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
-import com.openai.models.responses.Response;
-import com.openai.models.responses.ResponseCreateParams;
 import org.springframework.stereotype.Component;
 
 @Component
